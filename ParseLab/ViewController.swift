070a45b1cd6e07a82f640dbabde5d0d50356d0d2
Aayush Gupta <aayush.gupta.bison.howard.edu>
2018-02-25 14:15:14 -0500

    Initial Commit.

---
+++ ParseLab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParseLab
+//
+//  Created by Pujan Thapa on 2/25/18.
+//  Copyright © 2018 Aayush. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
