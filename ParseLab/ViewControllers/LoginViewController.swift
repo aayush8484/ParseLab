b3a5e2da1c02060be00719173f5befa346f84e7c
Aayush Gupta <aayush.gupta.bison.howard.edu>
2018-02-25 14:40:52 -0500

    Basic Login Page Setup.

---
+++ ParseLab/ViewControllers/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  ParseLab
+//
+//  Created by Pujan Thapa on 2/25/18.
+//  Copyright © 2018 Aayush. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameLabel: UITextField!
+    @IBOutlet weak var passwordLabel: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ParseLab' do
+  pod 'Parse'
+     post_install do |installer|
+       installer.pods_project.targets.each do |target|
+         target.build_configurations.each do |config|
+           config.build_settings['SWIFT_VERSION'] = '3.2'
+         end
+       end
+     end
+  target 'ParseLabTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ParseLabUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
